4cbb0155073fa12ed38ae9db93a28c306585ed30
Paweł <zzneix@gmail.com>
2019-07-10 13:39:33 +0200

    added mysql and talkedRecently set in index
    also rewamped auth.json into auth.js with database credentials

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //npm libs
 const Discord = require('discord.js'); //discord core library
 const enmap = require('enmap'); //enmap object for command handler
-const fs = require('fs');
+const fs = require('fs'); //filesystem module
 const schedule = require('node-schedule'); //yet useless
+const mysql = require('mysql2/promise'); //library for communicating with MySQL database (/peomise is for async/await)
 require('npm-package-to-env').config(); //importing version value from package.json
 require(`./extensions/errorHandler`); //handling thrown errors
 
@@ -10,23 +11,34 @@ var Promise = require('bluebird'); //module for error handler and rejections whi
 Promise.config({longStackTraces:true}); //enabling long stack trees
 
 //JSON data
-const config = require(`./src/json/config.json`);
-const perms = require(`./src/json/perms.json`);
+const config = require('./src/json/config.json');
+const perms = require('./src/json/perms.json');
+const auth = require('./src/json/auth')(Promise);
 
-//Discord client extras
-const client = new Discord.Client();
-client.config = config;
-client.perms = perms;
-client.commands = new enmap();
+
+//global Discord client extras
+const client = new Discord.Client(); //declaring new Discord client user as a global variable
+client.config = config; //global config
+client.perms = perms; //global permission set export
+client.auth = auth; //global authentication credentials
+client.commands = new enmap(); //declaring enmap for command handler
 client.version = process.env.npm_package_version; //global version
 client.RC = require('reaction-core'); //module for ahndling reactions
-client.RCHandler = new client.RC.Handler;
-client.fs = fs;
+client.RCHandler = new client.RC.Handler; //reaction-core handler
+client.fs = fs; //global filesystem module
 client.schedule = schedule; //yet useless
+client.mysql = mysql; //global mysql lib object (just in case)
+client.tr = new Set(); //set for talkedRecently (used by lidl leveling system)
+// client.dbConn = require(`./src/functions/mysqlConnect`)(mysql, auth); //global database connection
 
-//handlers
+//mysql database connaction establish thingy
+// let conn = client.mysql.createConnection(client.auth.db);
+// client.conn = conn;
+
+//handlers and global function exports
 require(`./src/functions/loadEvents`)(client); //event handler
 require(`./src/functions/loadCommands`)(client); //command handler
+// client.query = async function query() {return await mysql.createConnection(auth.db);}
 
 //Discord authentication
-client.login(process.env.token);
+client.login(process.env.token); //with Discord bot token
