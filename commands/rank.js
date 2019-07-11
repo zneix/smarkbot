c148dc78aed8f7304a83952b29f90f0af8988c60
Paweł <zzneix@gmail.com>
2019-07-11 18:47:12 +0200

    redone methoeds in rank.js ^^

--- commands/rank.js
+++ commands/rank.js
@@ -12,10 +12,10 @@ exports.run = async (client, message) => {
         setTimeout(function(){client.lvlcd.delete(message.author.id)}, 100);
 
         //actual response
-        const conn = await client.mysql.createConnection(client.auth.db);
-        const [rows, fields] = await conn.execute(`SELECT * FROM \`smarkbot_levels\` WHERE uid = ${message.author.id}`);
         if (!rows.length) return [
             await conn.execute(`INSERT INTO \`smarkbot_levels\` (pk, uid, xp, lvl) VALUES (NULL, ${message.author.id}, 0, 0)`),
+        var conn = await client.mysql.createConnection(client.auth.db);
+        var [rows, fields] = await conn.query(`SELECT * FROM \`smarkbot_levels\` WHERE uid = ${message.author.id}`);
             require(`../src/embeds/rankCheck`)(message, rows[0]["xp"], rows[0]["lvl"])
         ]
         require(`../src/embeds/rankCheck`)(message, rows[0]["xp"], rows[0]["lvl"])
