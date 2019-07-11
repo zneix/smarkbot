27b49c07824dcb4a4609fff251a2666e81bef71c
Paweł <zzneix@gmail.com>
2019-07-11 03:22:58 +0200

    added rank check command

---
+++ commands/rank.js
@@ -0,0 +1,23 @@
+exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
+exports.description = `Allows you to check your level progress (has 60s cooldown).`;
+exports.usage = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`
+exports.perms = `user`
+
+exports.run = async (client, message) => {
+    message.command(false, async () => {
+        if (client.lvlcd.has(message.author.id)) throw "You can use that command once per **60 seconds**!\nTry again later."
+
+        //cooldown thingy
+        client.lvlcd.add(message.author.id);
+        client.at('in 60 seconds', function(){client.lvlcd.delete(message.author.id)});
+
+        //actual response
+        const conn = await client.mysql.createConnection(client.auth.db);
+        const [rows, fields] = await conn.execute(`SELECT * FROM \`smarkbot_levels\` WHERE uid = ${message.author.id}`);
+        if (!rows.length) return [
+            await conn.execute(`INSERT INTO \`smarkbot_levels\` (pk, uid, xp, lvl) VALUES (NULL, ${message.author.id}, 0, 0)`),
+            require(`../src/embeds/rankCheck`)(client, message, rows[0]["xp"], rows[0]["lvl"])
+        ]
+        require(`../src/embeds/rankCheck`)(client, message, rows[0]["xp"], rows[0]["lvl"])
+    });
+}
---
+++ src/embeds/rankCheck.js
@@ -0,0 +1,42 @@
+module.exports = (client, message, xp, lvl) => {
+    function reqXP(){
+        let sum = 0;
+        let i = 0;
+        do {
+            sum = sum + (5 * Math.pow(i, 2) + 50 * i + 100);
+            i++;
+        } while (i < lvl+1);
+        return sum;
+    }
+    function actualXP(){
+        let sum = 0;
+        let i = 0;
+        do {
+            sum = sum + (5 * Math.pow(i, 2) + 50 * i + 100);
+            i++;
+        } while (i < lvl);
+        return sum;
+    }
+    var embed = {
+        color: 0xe7f135,
+        timestamp: new Date(),
+        footer: {
+            text: message.author.tag,
+            icon_url: message.author.avatarURL
+        },
+        description: "Level progress",
+        fields: [
+            {
+                name: "Level",
+                value: lvl,
+                inline: true
+            },
+            {
+                name: "Experience",
+                value: xp-actualXP()+"/"+(reqXP()-actualXP())+` (total: ${xp})`,
+                inline: true
+            }
+        ]
+    }
+    return message.channel.send({embed:embed});
+}
