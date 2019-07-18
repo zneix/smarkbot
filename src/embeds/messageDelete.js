02c6efeb8475f1b0a059ea447eb2b707124d213f
Paweł <zzneix@gmail.com>
2019-07-18 13:48:28 +0200

    added messageDelete logging

---
+++ src/embeds/messageDelete.js
@@ -0,0 +1,35 @@
+module.exports = (client, message, channel) => {
+    function leadingZeroes(n){
+        if (n<=9) return "0"+n;
+        return n;
+    }
+    function formattedMs(n){
+        if (n<=60000) return `${Math.floor(n/1000)}s`;
+        if (n<=3600000) return `${Math.floor(n/60000)}m ${Math.floor(n/1000)-(Math.floor(n/60000)*60)}s`;
+        if (n<=86400000) return `${Math.floor(n/3600000)}h ${Math.floor(n/60000)-(Math.floor(n/3600000)*60)}m`;
+        return `${Math.floor(n/86400000)}d ${Math.floor(n/3600000)-(Math.floor(n/86400000)*24)}h`;
+    }
+    let date = user.createdAt;
+    let formattedDate = leadingZeroes(date.getDate())+"/"+leadingZeroes(date.getMonth()+1)+"/"+date.getFullYear()+", "+leadingZeroes(date.getHours())+":"+leadingZeroes(date.getMinutes())+":"+leadingZeroes(date.getSeconds());
+    var embed = {
+        color: 0x2b2321,
+        timestamp: Date.now(),
+        thumbnail: user.avatarURL,
+        footer: {
+            text: `${user.id} | ${message.id}`,
+            icon_url: user.avatarURL
+        },
+        author: {
+            name: `Message Deleted`,
+            icon_url: user.avatarURL
+        },
+        description: `${message.author} in ${message.channel}`,
+        fields: [
+            {
+                name: "Deleted Message",
+                value: message.content
+            }
+        ]
+    }
+    return client.channels.get(channel.id).send({embed:embed});
+}
