d75cd2dce3c4c6087be5c63245350b24a4b36985
Paweł <zzneix@gmail.com>
2019-08-22 18:21:14 +0200

    initialized logging edited messages feature

---
+++ events/messageUpdate.js
@@ -0,0 +1,9 @@
+module.exports = (client, oldMessage, newMessage) => {
+    if (newMessage.channel.type === "dm") return;
+    if (newMessage.guild.id !== client.config.guildID) return;
+    if (newMessage.author.bot) return;
+
+    console.log(`[messageUpdate] message '${newMessage.id}' edited in '${newMessage.channel}'`);
+    let verLogs = client.channels.get(client.config.channels.verLogs);
+    if (verLogs) require(`../src/embeds/messageUpdate`)(client, oldMessage, newMessage, verLogs);
+}
