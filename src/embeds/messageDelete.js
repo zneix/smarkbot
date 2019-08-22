15aeb1c4ffe81ca7dce007733c7267168d329f5f
Paweł <zzneix@gmail.com>
2019-08-22 19:45:06 +0200

    purged unused functions

--- src/embeds/messageDelete.js
+++ src/embeds/messageDelete.js
@@ -1,8 +1,4 @@
 module.exports = (client, message, channel) => {
-    function leadingZeroes(n){
-        if (n<=9) return "0"+n;
-        return n;
-    }
     function formattedMs(n){
         if (n<=60000) return `${Math.floor(n/1000)}s`;
         if (n<=3600000) return `${Math.floor(n/60000)}m ${Math.floor(n/1000)-(Math.floor(n/60000)*60)}s`;
@@ -10,12 +6,9 @@ module.exports = (client, message, channel) => {
         return `${Math.floor(n/86400000)}d ${Math.floor(n/3600000)-(Math.floor(n/86400000)*24)}h`;
     }
     let user = message.author;
-    let date = user.createdAt;
-    let formattedDate = leadingZeroes(date.getDate())+"/"+leadingZeroes(date.getMonth()+1)+"/"+date.getFullYear()+", "+leadingZeroes(date.getHours())+":"+leadingZeroes(date.getMinutes())+":"+leadingZeroes(date.getSeconds());
     var embed = {
         color: 0x2b2321,
         timestamp: Date.now(),
-        thumbnail: user.avatarURL,
         footer: {
             text: `${user.id} | ${message.id}`,
             icon_url: user.avatarURL
