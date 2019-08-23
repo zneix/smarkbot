27789165af97aea6544ffa3e197c3ddd78919d08
Paweł <zzneix@gmail.com>
2019-08-23 10:27:41 +0200

    silly logging should be fixed now

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,7 +1,7 @@
 module.exports = (client, oldMessage, newMessage) => {
     if (newMessage.channel.type === "dm") return;
     if (newMessage.guild.id !== client.config.guildID) return;
-    if (newMessage.author.bot) return;
+    if (newMessage.author.bot || !newMessage.editedTimestamp) return;
 
     console.log(`[messageUpdate] message '${newMessage.id}' edited in '${newMessage.channel}'`);
     let verLogs = client.channels.get(client.config.channels.verLogs);
