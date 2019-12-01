225597f8f60982fe752a1d27023067dedd7dca7a
Paweł <44851575+zneix@users.noreply.github.com>
2019-12-01 02:14:00 +0100

    MongoDB > MySQL

--- commands/rank.js
+++ commands/rank.js
@@ -1,26 +1,20 @@
+let cooldown = 30000;
 exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
-exports.description = `Allows you to check your level progress (has 30s cooldown).`;
-exports.usage = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`
-exports.perms = `user`
-exports.home = true
+exports.description = `Allows you to check your level progress (has ${cooldown/1000}s cooldown).`;
+exports.usage = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
+exports.perms = 'user';
+// exports.home = true;
 
 exports.run = async (client, message) => {
     message.command(false, async () => {
-        if (client.lvlcd.has(message.author.id)) throw "You can use that command once per **30 seconds**!\nTry again later."
-
-        //cooldown thingy
+        //lidl command cooldown
+        if (client.lvlcd.has(message.author.id)) throw `You can only use that command once per **${cooldown/1000} seconds**!`;
         client.lvlcd.add(message.author.id);
-        setTimeout(function(){client.lvlcd.delete(message.author.id)}, 30000);
+        setTimeout(function(){client.lvlcd.delete(message.author.id)}, cooldown);
 
         //actual response
-        // var conn = await client.mysql.createConnection(client.auth.db);
-        // var [rows, fields] = await conn.query(`SELECT * FROM \`smarkbot_levels\` WHERE uid = ${message.author.id}`);
-        // if (!rows.length) {
-        //     await conn.query(`INSERT INTO \`smarkbot_levels\` (pk, uid, xp, lvl) VALUES (NULL, ${message.author.id}, 0, 0)`),
-        //     await require(`../src/embeds/rankCheck`)(message, rows[0]["xp"], rows[0]["lvl"])
-        //     return conn.destroy();
-        // }
-        // await require(`../src/embeds/rankCheck`)(message, rows[0]["xp"], rows[0]["lvl"])
-        // return await conn.destroy();
+        let userLvl = (await client.db.lvl.findUser(message.guild.id, message.author.id))[0];
+        if (!userLvl) (await client.db.lvl.newUser(message.guild.id, message.author.id))[0];
+        require(`../src/embeds/rankCheck`)(message, userLvl);
     });
 }
